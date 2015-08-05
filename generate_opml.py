1284bbd80f03c59d3e0292c59cb3d0f668f6e106
rushter <me@rushter.com>
2015-08-05 17:23:56 +0300

    Add opml generator

---
+++ generate_opml.py
@@ -0,0 +1,29 @@
+import re
+
+xmlbody = """
+<?xml version="1.0" encoding="UTF-8"?>
+<opml version="1.0">
+    <head>
+        <title>Data science blogs</title>
+    </head>
+    <body>
+        <outline text="Data science blogs" title="Data science blogs">
+        {items}
+        </outline>
+    </body>
+</opml>
+    """
+xmlitem = '<outline type="rss" text="{title}" title="{title}" ' \
+          'xmlUrl="{rssfeed}" htmlUrl="{httpfeed}"/>'
+
+readme = open('README.md').read()
+blogs = re.findall('\* (.*?) http([s]{0,1})\:\/\/(.*?) \[\(RSS\)\] \((.*?)\)', readme)
+
+items = ''
+for blog in blogs:
+    item = xmlitem.format(title=blog[0].strip(),
+                          httpfeed='http{0}://{1}'.format(blog[1].strip(), blog[2].strip()),
+                          rssfeed=blog[3].strip())
+    items += item + '\r\n'
+
+open('data-science.opml', 'w').write(xmlbody.format(items=items))
